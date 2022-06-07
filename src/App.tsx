79dca65220432002f3c70f13f4380f0b56b0bfa7
Michael Schlueter <schluet0r4life@gmail.com>
2022-06-07 12:48:48 +0200

    implement react-router, set up basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { CrewPage } from './pages/CrewPage';
+import { DestinationPage } from './pages/DestinationPage';
+import { MainPage } from './pages/MainPage';
+import { TechnologyPage } from './pages/TechnologyPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="crew" element={<CrewPage />} />
+        <Route path="destination" element={<DestinationPage />} />
+        <Route path="technology" element={<TechnologyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
