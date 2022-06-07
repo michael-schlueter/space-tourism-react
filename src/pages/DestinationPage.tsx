ee25f1bb01d78a1f6722936093f976efefde47d5
Michael Schlueter <schluet0r4life@gmail.com>
2022-06-07 12:17:49 +0200

    implement app structure

---
+++ src/pages/DestinationPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DestinationPage = () => {
+  return (
+    <div>DestinationPage</div>
+  )
+}
---
+++ src/pages/TechnologyPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TechnologyPage = () => {
+  return (
+    <div>TechnologyPage</div>
+  )
+}
